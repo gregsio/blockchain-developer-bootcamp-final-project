c9006072bffaac148be20576e0600feebc17c69a
Gregs <github@gregs.io>
2021-11-06 23:13:03 -0400

    MVP functions implemented with unit tests

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -3,17 +3,19 @@ pragma solidity 0.8.9;
 
 contract Crowdfunding {
 
-    //constructor() public {}
+  address public contractOwner;
+  uint256 public campaignBalance = 0 ether;
+  uint256 public constant requiredMinFunds = 3 ether;
+  uint256 public constant targetGoal = 12 ether;
 
-    // The website displays: balance of funds donated, minimum funds required to kick-off the project/cause, an optimal target goal.
-    function campaignstatus() public view returns(uint campaignbalance, uint minfunds, uint targetgoal) {
-
+    constructor() {
+      contractOwner = msg.sender;
     }
 
-
     //Users willing to participate in the crowdfunding click on the "Donate" button. Metamask authentication triggers and allow the user to send ETH to the contact address.
-    function deposit() public payable returns(uint256 balance){
-
+    function deposit() public payable {
+      campaignBalance += msg.value;
+      emit depositEvent(msg.sender, msg.value);
     }
 
     // Upon succesful transaction the balance updates on the website.
@@ -22,15 +24,22 @@ contract Crowdfunding {
 
     // Contract/campaign owner can withdraw or refund funds.
     modifier owner() {
+      require(msg.sender == contractOwner, "Not owner");
       _;
     }
-    function withdrawfunds() public owner {
-
+    function withdrawFunds() public payable owner {
+      uint256 withdrawAmout = campaignBalance;
+      campaignBalance = 0;
+      (bool sent, bytes memory data) = contractOwner.call{value: withdrawAmout}("");
+      require(sent, "Failed to send Ether");
+      emit withdrawEvent(withdrawAmout, contractOwner);
     }
+
+    event withdrawEvent(uint256 amount, address owner);
+
     function refund(address donor) public owner {
 
     }
-    event withdrawEvent(uint256 amount);
 
 
 
--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -9,13 +9,94 @@ contract("Crowdfunding", function (accounts) {
 
   const [_owner, alice, bob] = accounts;
   let instance;
+  let tryCatch = require("./exceptions.js").tryCatch;
+  let errTypes = require("./exceptions.js").errTypes;
 
   beforeEach(async () => {
     instance = await Crowdfunding.new();
   });
+  describe("Deployment", () => {
+    it("should assert true", async function () {
+      await Crowdfunding.deployed();
+      return assert.isTrue(true);
+    });
+  });
 
-  it("should assert true", async function () {
-    await Crowdfunding.deployed();
-    return assert.isTrue(true);
+  describe("State Variables", () => {
+    it("should have a campaignBalance", async () => {
+      assert.equal(typeof instance.campaignBalance, 'function', "the contract has no campaignBalance");
+    });
+    it("should have requiredMin", async () => {
+      assert.equal(typeof instance.requiredMinFunds, 'function', "the contract has no requiredMinFunds");
+    });
+    it("should have a targetGoal", async () => {
+      assert.equal(typeof instance.targetGoal, 'function', "the contract has no targetGoal");
+    });
+  });
+
+  describe("Use cases", () => {
+    let campaignBalance = web3.utils.toWei('0');
+    let requiredMinFunds = web3.utils.toWei('3');
+    let targetGoal = web3.utils.toWei('12');
+
+    it("should retrieve the current campaignBalance, requiredMin and targetGoal", async () => {
+      var result = await instance.campaignBalance();//.then(console.log);
+      assert.equal(
+        result,
+        campaignBalance,
+        "the campaignBalance does not match the expected value",
+      );
+      result = await instance.requiredMinFunds();
+      assert.equal(
+        result,
+        requiredMinFunds,
+        "the requiredMinFunds does not match the expected value",
+      );
+      result = await instance.targetGoal();
+      assert.equal(
+        result,
+        targetGoal,
+        'the targetGoal does not match the expected value',
+      );
+    });
+
+    it("should deposit ETH to the campaign", async () => {
+      var deposit = await instance.deposit({ from: alice, value: 1});
+      const result = await instance.campaignBalance();
+      assert.equal(
+        web3.utils.toWei(result),
+        web3.utils.toWei('1'),
+        "the campaignBalance does not match the expected value",
+      )
+    });
+    it("should emit a deposit event", async () => {
+      const result = await instance.deposit({ from: alice, value: 1});
+      assert.equal(
+        result.logs[0].event == 'depositEvent',
+        true,
+        "deposit event has not been emitted",
+      )
+    });
+
+    it("should allow the contract owner to withdraw funds", async () => {
+      await instance.deposit({from: alice, value: 1});
+      await instance.withdrawFunds({ from: _owner });
+      const campaignBalance = await instance.campaignBalance();
+      assert.equal(campaignBalance, 0, "withdrawing funds was not sucessful")
+    });
+
+    it("should emit a withdraw event", async () => {
+      await instance.deposit({from: alice, value: 1});
+      result = await instance.withdrawFunds({ from: _owner });
+      assert.equal(
+        result.logs[0].event == 'withdrawEvent',
+        true,
+        "withdraw event has not been emitted",
+      )
+    });
+    it("should only allow the contract owner to withdraw funds", async () => {
+      await instance.deposit({from: alice, value: 1});
+      await tryCatch(instance.withdrawFunds({from: bob}), errTypes.revert);
+    });
   });
 });
